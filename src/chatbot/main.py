2d8b12606dd9c9c977cffd46f6ef04d63843defc
Carlos Dávila <dev.carlosdavila@gmail.com>
2025-05-12 03:13:38 -0500

    change model

--- src/chatbot/main.py
+++ src/chatbot/main.py
@@ -20,7 +20,8 @@ if not firebase_admin._apps:
 db = firestore.client()
 
 # Cargar modelo de Hugging Face
-model_name = "NadiaLiz/Llama-3.2"
+model_name = "csdavila/Llama-3.2.B.S2"
+# model_name = "NadiaLiz/Llama-3.2"
 tokenizer  = AutoTokenizer.from_pretrained(model_name)
 device     = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model      = AutoModelForCausalLM.from_pretrained(
